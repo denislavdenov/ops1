6978e639a3539143ef07ae5b63710ffe2e4f9498
denislavdenov <42144642+denislavdenov@users.noreply.github.com>
2018-08-15 09:44:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,17 +9,35 @@ provider "github" {
   organization = "FromBeginning"
 }
 
-resource "github_repository" "example5" {
-  name        = "example5"
+# 2 repos with hardcoded names
+resource "github_repository" "example1" {
+  name        = "example1"
   description = "My awesome codebase"
 }
 
-resource "random_pet" "repo" {
+resource "github_repository" "example2" {
+  name        = "example2"
+  description = "My awesome codebase"
+}
+
+# We use random_pet to generate a random name
+resource "random_pet" "random_repo1" {
   length    = "4"
   separator = "-"
 }
 
-resource "github_repository" "example2" {
-  name        = "${random_pet.repo.id}"
+resource "random_pet" "random_repo2" {
+  length    = "4"
+  separator = "-"
+}
+
+# We generate 2 dynamic repos with the generated random names
+resource "github_repository" "random1" {
+  name        = "${random_pet.random_repo1.id}"
+  description = "My awesome pet project"
+}
+
+resource "github_repository" "random2" {
+  name        = "${random_pet.random_repo2.id}"
   description = "My awesome pet project"
 }
